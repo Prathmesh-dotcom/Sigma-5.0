df6c2957ddf871cae3f4160705f9b4790d2a2f69
Prathmesh dotcom <prathmeshasathe@gmail.com>
2024-11-10 11:50:08 +0530

    new files added

---
+++ PosiNega.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class PosiNega {
+    public static void main(String args[]){
+        System.out.println("Enter a Number");
+        Scanner sc= new Scanner(System.in);
+        int num= sc.nextInt();
+        if (num>=0) {
+            System.out.println("Number is Positive");
+        }
+        else{
+            System.out.println("Number is Negative");
+        }
+
+    }
+}
