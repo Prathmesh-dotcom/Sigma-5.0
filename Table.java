334bb0de10dd4fa03af8a7bffe3c3eb638a8f776
Prathmesh dotcom <prathmeshasathe@gmail.com>
2024-12-07 22:52:11 +0530

    .

---
+++ Table.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Table {
+    public static void MultiplicationTable(int num) {
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Enter a number");
+        int n= sc.nextInt();
+
+        for(int i=1; i<=10; i++){
+            System.out.println(n+" * "+i+" = "+n*i);
+        }
+    }
+    public static void main(String s[]){
+        MultiplicationTable(5);
+    }
+}
