b995d38f7eec6d9e6712c0658b833cd4dcfcb3a1
Prathmesh dotcom <prathmeshasathe@gmail.com>
2024-12-09 16:49:30 +0530

    Using scanner class

--- Bill.java
+++ Bill.java
@@ -1,6 +1,7 @@
 import java.util.*;
 public class Bill {
     public static void main(String args[]){
+        
         Scanner sc= new Scanner(System.in);
         System.out.println("Enter price of pencil");
         float pencil= sc.nextFloat();
