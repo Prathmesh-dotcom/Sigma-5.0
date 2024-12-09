2fd51791565065a255465064538cde8c04e696ca
Prathmesh dotcom <prathmeshasathe@gmail.com>
2024-12-09 16:45:40 +0530

    using function

---
+++ Mulpication.java
@@ -0,0 +1,12 @@
+public class Mulpication {
+    public static int Multiply(int a, int b){
+        int ans=a*b;
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        int ans= Multiply(5, 10);
+        System.out.println("Answer is: "+ans);
+        
+    }
+}
