2f0a13a56a5c08d1f82c0cd648c110e6a54e121d
Prathmesh dotcom <prathmeshasathe@gmail.com>
2024-12-09 16:54:40 +0530

    By using nested loop

--- InvertedStarPattern.java
+++ InvertedStarPattern.java
@@ -5,6 +5,7 @@ public class InvertedStarPattern {
             for(int j=1; j<=n-i+1; j++){
                 System.out.print("*");
             }
+            
             System.out.println();
         }
     }
